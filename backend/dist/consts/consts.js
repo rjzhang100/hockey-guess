7fb68a06956f06d8447693799efea079ad916c7d
rjzhang100 <78387172+rjzhang100@users.noreply.github.com>
2025-03-26 15:54:50 -0700

    oops committed dist

--- backend/dist/consts/consts.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ObjectIdRegex = exports.CONSTS = void 0;
-var CONSTS;
-(function (CONSTS) {
-    CONSTS["TOKEN_COOKIE_KEY"] = "token";
-})(CONSTS || (exports.CONSTS = CONSTS = {}));
-exports.ObjectIdRegex = /^[a-fA-F0-9]{24}$/;
